50efc725ab2b21904ab4bd46e512e623b378549f
d3wp <d3wp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 22:33:29 +0000

    version 1.0 commit
    git-svn-id: https://plugins.svn.wordpress.org/plugins-speed-test/tags/1.0@1119224 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,58 @@
+=== Plugins Speed Test ===
+Contributors: d3wp
+Donate link: http://www.wpspeedster.com
+Tags: plugin,plugin speed test
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin shows impact of installed plugins on your blogs' speed.
+
+== Description ==
+This used, shows impact of installed plugins (only for those located in the official WordPress plugin repository) on your blogs'
+speed.  In other words, with this plugin, you can easily determine which plugins from the WordPress
+plugin repository are less resource intensive, and which ones can slow down your WordPress blog when used.
+
+The plugin shows four results (see screenshot):
+
+* Impact of the installed plugin on Google PageSpeed score for blogs' Home page
+* Impact of the installed plugin on Google PageSpeed score for blogs' sample Post page
+* Resources added to post page after plugin has been installed (in kB)
+* Number of DB tables created by the plugin.
+
+More than 25.000 plugins were tested lately.
+
+Every plugin was tested on a fresh WordPress installed instance populated with a sample content.
+No additional plugins were installed to avoid any unwanted impact on the speed test.
+
+NOTE:
+This plugin calls [wpspeedster.com](http://www.wpspeedster.com) API to obtain necessary information about Speed Impact details.
+Only plugin slugs are send to the API, no additional or personal information is send.
+
+== Installation ==
+
+1. Upload `plugins-speed-test` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to ‘Plugins -> Installed Plugins’ to view "Speed Impact" column in plugins list table.
+
+== Frequently Asked Questions ==
+
+= How many plugins were tested? =
+
+More than 20.000 plugins were tested.
+
+== Screenshots ==
+
+1. "Speed Impact" column in plugins list table.
+
+== Changelog ==
+
+= 1.0 =
+* Initial version.
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Initial version.
---
+++ plugins-speed-test.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ * @link              http://www.wpspeedster.com
+ * @since             1.0
+ * @package           Plugins_Speed_Test
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Plugins Speed Test
+ * Plugin URI:        http://blog.wpspeedster.com/plugins-speed-test-plugin/
+ * Description:       This plugin shows resource impact of specific plugins on your blog.
+ * Version:           1.0
+ * Author:            Csaba Kissi
+ * Author URI:        http://www.wpspeedster.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugins-speed-test
+ * Domain Path:       /languages
+ */
+
+register_activation_hook(__FILE__, 'activate_plugins_speed_test');
+register_deactivation_hook(__FILE__, 'deactivate_plugins_speed_test');
+
+function activate_plugins_speed_test()
+{
+}
+
+function deactivate_plugins_speed_test()
+{
+}
+
+add_action( 'admin_enqueue_scripts', 'wp_pst_plugin_scripts_init' );
+add_filter( 'manage_plugins_columns', 'wp_pst_add_plugins_column' );
+add_action( 'manage_plugins_custom_column' , 'wp_pst_render_plugins_column', 10, 3 );
+
+function wp_pst_plugin_scripts_init($hook) {
+    if($hook != 'plugins.php') return;
+    wp_register_script('wp_pst_plugin-script', plugins_url( 'assets/js/script.js', __FILE__ ),array('jquery'));
+    wp_enqueue_script('wp_pst_plugin-script');
+    wp_enqueue_style( 'wp_pst_plugin-style', plugins_url( 'assets/css/style.css', __FILE__ ));
+}
+
+function wp_pst_add_plugins_column( $columns ) {
+    $columns['wp_pst_column'] = 'Speed Impact';
+    return $columns;
+}
+
+
+function wp_pst_render_plugins_column( $column_name, $plugin_file, $plugin_data ) {
+    if ( 'wp_pst_column' == $column_name ) :
+        ?>
+        <div class="plugin-description">
+            <table class="wp_pst">
+                <tr><td title="Speed Impact on Blogs' Home Page">Home Page Impact:</td><td  style="margin:0"><span id="hp_<? echo $plugin_data['slug']?>">-</span></td></tr>
+                <tr><td title="Speed Impact on Blogs' Post Page">Post Page Impact:</td><td><span id="pp_<? echo $plugin_data['slug']?>">-</span></td></tr>
+                <tr><td title="Size of the resources added (in kB)">Resources added:</td><td><span id="rs_<? echo $plugin_data['slug']?>">-</span></td></tr>
+                <tr><td title="Number of Database tables created">DB tables:</td><td><span id="db_<? echo $plugin_data['slug']?>">-</span></td></tr>
+            </table>
+            <div class="row-actions visible"><span class="activate"><a href="http://www.wpspeedster.com/plugin/speedtest/<?php echo $plugin_data['slug'] ?>" title="More Speed Test Details" class="edit">More Details</a></span></div>
+        </div>
+    <?php
+    endif;
+}
